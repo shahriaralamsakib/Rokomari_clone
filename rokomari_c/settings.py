ddbfdf8b2134e30610714c918de482d2256b14e5
Sakib <shahriaralamsakib7039@gmail.com>
2023-12-24 20:21:38 +0600

    Added PostgeSQL Database

--- rokomari_c/settings.py
+++ rokomari_c/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'rokomari_c.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "rokomari_c",
+        "USER": "sakib",
+        "PASSWORD": "sakib",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
