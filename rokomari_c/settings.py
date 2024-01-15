3b52c96533ba291a4de4732036c0fed09bb1b28b
Sakib <shahriaralamsakib7039@gmail.com>
2024-01-15 22:11:45 +0600

    added two app oder and inventory

--- rokomari_c/settings.py
+++ rokomari_c/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'order',
+    'inventory',
     'rest_framework',
     'rest_framework.authtoken',
 ]
--- rokomari_c/urls.py
+++ rokomari_c/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('order/', include('order.urls')),
+    path('inventory/', include('inventory.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
